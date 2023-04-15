9fc2f457a985640cc2f317c8ec145bcee098b864
Mr. Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-04-15 15:32:01 +0500

    My First NextJS 13 app, using Chakra-UI Templates.

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import HeroSection from '@/components/HeroSection'
+import Features from '@/components/Features'
+import Testimonials from '@/components/Testimonials'
+
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      {/* <h1>Home</h1> */}
+      <HeroSection/>
+      <Features/>
+      <Testimonials/>
+    </>
+  )
+}
