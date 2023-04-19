82d0777c284eee537fe199c1d1a53f63e9dea9be
Mr. Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-04-19 20:56:14 +0500

    updated title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      	<title>Chakra-UI Templates</title>
+      </head>
       <body>
         <ChakraProvider>
           <Navbar/>
--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,6 @@ import Features from '@/components/Features'
 import Testimonials from '@/components/Testimonials'
 
 
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <>
